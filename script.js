058f5e3104a09e1d2a417b0ad249865d65e73791
farhan-sqb <91463169+farhan-sqb@users.noreply.github.com>
2021-12-16 13:45:25 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,47 +1,89 @@
 let todayDate = new Date();
 
-function callAPI(){
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+
+    if(h >= 5 && h < 12){
+        document.getElementById("greetings").innerHTML = "Good Morning";
+    }else if(h >= 12 && h < 18){
+        document.getElementById("greetings").innerHTML = "Good Afternoon";
+    }else{
+        document.getElementById("greetings").innerHTML = "Good Evening";
+    }
     
+    document.getElementById('clockSeconds').innerHTML =  h + ":" + m + ":" + s;
+    document.getElementById('clockwithoutSeconds').innerHTML =  h + ":" + m;
+    
+    setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+        return i;
+}
+
+window.onload = function(){
+    startTime();
+
     fetch('https://api.nasa.gov/planetary/apod?api_key=8NxQeyomdZwXICeJHvWdnDWOnoiwpzNnZQJhOgZe&count=2')
         .then((response) => {
             return response.json();
         })
         .then((myJson) => {
-            console.log(myJson[0]['hdurl']);
+            console.log(myJson);
             document.getElementById("potd").src = myJson[0]['hdurl'];
             
-            let dPattern = localStorage.getItem("datePattern");
+            document.getElementById("topLeft").innerHTML = todayDate;
+            
+            const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+            document.getElementById("dayofWeek").innerHTML = "Today Is:- " + weekday[todayDate.getDay()];
 
-            if(dPattern == 1){
-                document.getElementById("topLeft").innerHTML = todayDate;
-            }else if(dPattern == 2){
-                document.getElementById("topLeft").innerHTML = todayDate.getFullYear() + "-" + todayDate.getMonth() + "-" + todayDate.getDay(); 
-            }
+            document.getElementById("dayofMonth").innerHTML = "Day Of Month Is:- " + todayDate.getDate();
+
+            let now = new Date();
+            let start = new Date(now.getFullYear(), 0, 0);
+            let diff = now - start;
+            let oneDay = 1000 * 60 * 60 * 24;
+            let day = Math.floor(diff / oneDay)
+            document.getElementById("dayofYear").innerHTML = "Day Of The Year Is:- " + day;
         });
 
 }
 
-function showDetails(){
+document.getElementById('btnMore').addEventListener('click', function() {
+    document.getElementById("moreItems").style.display = "block";
+});
 
-    let item = localStorage.getItem("showItem");
+document.getElementById('btnSettings').addEventListener('click', function() {
+    document.getElementById("pref_1").style.display = "block";
+    document.getElementById("pref_2").style.display = "block";
+    document.getElementById("pref_3").style.display = "block";
 
-    if(item == 2){
-        const monthNames = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("settings").style.display = "none";
+});
 
-        let currentMonth = monthNames[todayDate.getMonth()];
-        document.getElementById("topLeft1").innerHTML = currentMonth;
-    }else if(item == 1){
-        const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    
-        let dayName = days[todayDate.getDay()];
-        document.getElementById("topLeft1").innerHTML = dayName;
+document.getElementById('btnSave').addEventListener('click', function() {
+    document.getElementById("greetings").style.color = document.querySelector('input[name="setColor"]:checked').value;
+    document.getElementById("dayofWeek").style.color = document.querySelector('input[name="setColor"]:checked').value;
+    document.getElementById("dayofMonth").style.color = document.querySelector('input[name="setColor"]:checked').value;
+    document.getElementById("dayofYear").style.color = document.querySelector('input[name="setColor"]:checked').value;
+
+    if(document.querySelector('input[name="setClock"]:checked').value == "Show Seconds"){
+        document.getElementById("clockSeconds").style.display = "block";
+        document.getElementById("clockwithoutSeconds").style.display = "none";
+    }else if(document.querySelector('input[name="setClock"]:checked').value == "Hide Seconds"){
+        document.getElementById("clockSeconds").style.display = "none";
+        document.getElementById("clockwithoutSeconds").style.display = "block";
     }
-}
 
-function setStorage(type){
-    localStorage.setItem("showItem", type);
-}
+    document.getElementById("pref_1").style.display = "none";
+    document.getElementById("pref_2").style.display = "none";
+    document.getElementById("pref_3").style.display = "none";
 
-function setDateStorage(type){
-    localStorage.setItem("datePattern", type);
-}
+    document.getElementById("settings").style.display = "block";
+});
